5a8aea51a7c4d901c54a935ea92193c2174de835
Samantha Wu <samanthayywu@gmail.com>
2022-06-21 14:03:59 +0100

    linked tables

--- db/scripts/createDiaryTable.js
+++ db/scripts/createDiaryTable.js
@@ -2,6 +2,8 @@ import { query } from "../index.js";
 
 let createSql = `CREATE TABLE IF NOT EXISTS diary (diary_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   week INT,
+  FOREIGN KEY(week_id)
+  REFERENCES progress(week_id),
   diary TEXT)`;
 
 async function createDiaryTable() {
