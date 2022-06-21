055aeb512b7bf631ef1b4ad46c5d08fb529b73ba
Samantha Wu <samanthayywu@gmail.com>
2022-06-21 10:10:13 +0100

    all routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,35 +1,22 @@
 import express from "express";
 const router = express.Router();
+
+import {} from "../models/models.js";
+
+router.get("/", async function (req, res) {});
+
+router.get("/:id", async function (req, res) {});
+
+router.get("/:name", async function (req, res) {});
+
+router.post("/", async function (req, res) {});
+
+router.put("/:id", async function (req, res) {});
+
+router.put("/:name", async function (req, res) {});
+
+router.delete("/:id", async function (req, res) {});
+
+router.delete("/:name", async function (req, res) {});
+
 export default router;
-
-import {
-  createCat,
-  getAllCats,
-  getCatByID,
-  getCatByName,
-  // deleteCat,
-  // updateCat
-} from "../models/models.js";
-
-router.get("/", async function (req, res) {
-  let nameQuery = req.query.name;
-  if (nameQuery) {
-    console.log("Fetching cat by nameQuery...");
-    res.json(await getCatByName(nameQuery));
-  } else {
-    console.log("Fetching all cats...");
-    res.json(await getAllCats());
-  }
-});
-
-router.get("/:id", async function (req, res) {
-  let id = Number(req.params.id);
-  console.log("Fetching cat by ID...");
-  res.json(await getCatByID(id));
-});
-
-router.post("/", async function (req, res) {
-  let newCat = req.body;
-  console.log("Creating new cat...");
-  res.json(await createCat(newCat));
-});
