6b1934ee79eb2a444b9abafe74e3d26f65c24e2c
Samantha Wu <samanthayywu@gmail.com>
2022-06-21 11:07:52 +0100

    my changes

--- db/scripts/createDiaryTable.js
+++ db/scripts/createDiaryTable.js
@@ -1,6 +1,11 @@
 import { query } from "../index.js";
 
-let createSql = `CREATE TABLE IF NOT EXISTS diary (bootamper_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, diary TEXT, week INT )`;
+let createSql = `CREATE TABLE IF NOT EXISTS diary (diary_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  bootcamper_id INT,
+  FOREIGN KEY(bootcamper_id)
+  REFERENCES progress(bootcamper_id),
+  diary TEXT,
+  week INT)`;
 
 async function createDiaryTable() {
   const res = await query(createSql);
