4ca6b620945376236eec618109c38b8da2ad2afd
Samantha Wu <samanthayywu@gmail.com>
2022-06-21 10:20:01 +0100

    get all bootcampers

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 const router = express.Router();
 
-import {} from "../models/models.js";
+import { getAllBootcampers } from "../models/models.js";
 
-router.get("/", async function (req, res) {});
+router.get("/", async function (req, res) {
+  res.json({ sucess: true, payload: await getAllBootcampers() });
+});
 
 router.get("/:id", async function (req, res) {});
 
