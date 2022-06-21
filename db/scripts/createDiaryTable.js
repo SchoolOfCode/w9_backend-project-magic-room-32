d90579dc87275ba13727b532e0d6140aca6fb68b
Samantha Wu <samanthayywu@gmail.com>
2022-06-21 09:56:50 +0100

    fixed create diary

--- db/scripts/createDiaryTable.js
+++ db/scripts/createDiaryTable.js
@@ -1,9 +1,9 @@
 import { query } from "../index.js";
 
-let createSql = `CREATE TABLE IF NOT EXISTS diary (bootamper_id INT PRIMARY KEY ALWAYS GENERATED AS IDENTITY, diary TEXT, week INT )`;
+let createSql = `CREATE TABLE IF NOT EXISTS diary (bootamper_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, diary TEXT, week INT )`;
 
 async function createDiaryTable() {
   const res = await query(createSql);
-  console.log(res.command);
+  console.log(res.command, "table created");
 }
 createDiaryTable();
--- db/scripts/deleteDiaryTable.js
+++ db/scripts/deleteDiaryTable.js
@@ -1,9 +1,9 @@
 import { query } from "../index.js";
 
-const deleteSql = `DROP TABLE IF EXISTS diary `;
+const deleteSql = `DROP TABLE IF EXISTS diary; `;
 
 async function dropDiaryTable() {
   const res = await query(deleteSql);
-  console.log(res.rows[0]);
+  console.log(res.command, "table deleted");
 }
 dropDiaryTable();
