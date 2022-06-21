4263c628db8fb0d0ea7e9ca612aec5c733f81176
MP-360 <mp360git@gmail.com>
2022-06-21 16:39:44 +0100

    first pass @ joining front-end

--- app.js
+++ app.js
@@ -2,18 +2,28 @@ import express from "express";
 import router from "./routes/week.js";
 import cors from "cors";
 import logger from "morgan";
+import { submitResults } from "./models/models.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(logger("dev"));
-app.use("/week", router);
+
+app.use("/", router);
 
 app.get("/", (req, res) => {
   res.send("root route");
 });
+
+app.post ("/", async (req, res) => {
+  let response = await submitResults(req.body);
+  console.log("hello world");
+  console.log(response);
+  console.log(req.body);
+  res.send("finished");
+})
 
 // Serves Front-end
 // app.get("/", function (req, res) {
