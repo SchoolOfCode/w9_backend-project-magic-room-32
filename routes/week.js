0d3ce6aa6ce3d732791c5059998dffb7bb335989
Samantha Wu <samanthayywu@gmail.com>
2022-06-21 15:15:41 +0100

    get by id

--- routes/week.js
+++ routes/week.js
@@ -2,13 +2,15 @@ import express from "express";
 import { cloneElement } from "react";
 const router = express.Router();
 
-import { getAllBootcampers } from "../models/models.js";
+import { getWeekById } from "../models/models.js";
 
 router.get("/", async (req, res) => {
   res.json({ sucess: true, payload: await getAllBootcampers() });
 });
 
-router.get("/:id", async function (req, res) {});
+router.get("/:id", async function (req, res) {
+  res.json({ sucess: true, payload: await getWeekById(req.params.id) });
+});
 
 router.get("/bootcamper", async function (req, res) {});
 
