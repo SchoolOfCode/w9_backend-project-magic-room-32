4b0d9f59ea79740cb4cd73b204fad4b8718c61b0
Samantha Wu <samanthayywu@gmail.com>
2022-06-21 10:36:15 +0100

    cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import router from "./routes/bootcampers.js";
+import cors from "cors";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/bootcampers", router);
 
 app.get("/", (req, res) => {
