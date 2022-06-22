766bc5d98d015dffbb9e59bd55b4847a6341db1d
Samantha Wu <samanthayywu@gmail.com>
2022-06-22 15:05:39 +0100

    submitResult sql

--- models/models.js
+++ models/models.js
@@ -1,41 +1,32 @@
 import { query } from "../db/index.js";
 
-export async function getAllWeeks() {
-  const res = await query(`SELECT * FROM diary INNER JOIN progress`);
-  console.log(res.rows);
-  return res.rows;
-}
+// export async function getAllWeeks() {
+//   const res = await query(`SELECT * FROM diary INNER JOIN progress`);
+//   console.log(res.rows);
+//   return res.rows;
+// }
 
-export async function getWeekById(id) {
-  const res = await query(
-    `SELECT * FROM diary INNER JOIN progress
-     ON progress.week_id = diary.week
-    WHERE progress.week_id = $1 AND diary.week = $1;`,
-    [id]
-  );
-  console.log(res.rows[0]);
-  return res.rows[0];
-}
-
-// export async function submitResults(userResults) {
-//   let latestResults = await query(
-//     `BEGIN IF NOT EXISTS (SELECT * FROM progress WHERE weekNumber = weekNumber
-//       AND quizNumber = quizNumber
-//       AND correctAnswers = correctAnswers)
-//       BEGIN INSERT INTO progress (weekNumber, quizNumber, correctAnswers)
-//       VALUES ($1, $2, $3) END END;`,
-//     [userResults.weekNumber, userResults.quizNumber, userResults.correctAnswers]
+// export async function getWeekById(id) {
+//   const res = await query(
+//     `SELECT * FROM diary INNER JOIN progress
+//      ON progress.week_id = diary.week
+//     WHERE progress.week_id = $1 AND diary.week = $1;`,
+//     [id]
 //   );
-//   return latestResults;
+//   console.log(res.rows[0]);
+//   return res.rows[0];
 // }
 
 export async function submitResults(userResults) {
   let latestResults = await query(
     `INSERT INTO progress (weekNumber, quizNumber, correctAnswers)
-    VALUES ($1, $2, $3) WHERE NOT EXISTS (SELECT * FROM progress WHERE weekNumber = $1
-      AND quizNumber = $2
-      AND correctAnswers = $3);`,
+    VALUES ($1, $2, $3) ON CONFLICT DO NOTHING`,
     [userResults.weekNumber, userResults.quizNumber, userResults.correctAnswers]
   );
   return latestResults;
+}
+
+export async function getScore(id) {
+  const res = await query(`SELECT * FROM progress WHERE weekNumber = $1`, [id]);
+  return res.rows;
 }
