8efc175cdeff02b3f6a945fcd75d2d985227da4a
MP-360 <mp360git@gmail.com>
2022-06-22 10:06:43 +0100

    fin

--- db/scripts/createDiaryTable.js
+++ db/scripts/createDiaryTable.js
@@ -3,7 +3,7 @@ import { query } from "../index.js";
 let createSql = `CREATE TABLE IF NOT EXISTS diary (diary_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   week INT,
   FOREIGN KEY(week)
-  REFERENCES progress(week_id),
+  REFERENCES progress(answer_id),
   diary TEXT)`;
 
 async function createDiaryTable() {
--- models/models.js
+++ models/models.js
@@ -13,8 +13,8 @@ export async function getWeekById(id) {
 
 export async function submitResults(userResults) {
   let latestResults = await query(
-    ` INSERT INTO progress (quizNumber, correctAnswers) VALUES ($1, $2)`,
-    [userResults.quizNumber, userResults.correctAnswers]
+    ` INSERT INTO progress (weekNumber, quizNumber, correctAnswers) VALUES ($1, $2, $3)`,
+    [userResults.weekNumber, userResults.quizNumber, userResults.correctAnswers]
   );
   return latestResults;
 }
