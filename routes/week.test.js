91d51f2e4bc7e955fe7cb989244bc634a861a96c
Samantha Wu <samanthayywu@gmail.com>
2022-06-23 11:28:07 +0100

    post week test

--- routes/week.test.js
+++ routes/week.test.js
@@ -20,3 +20,16 @@ describe(`get weekly score and sends back percentage`, () => {
 });
 
 // post result by id
+
+describe(`post score by week`, () => {
+  test(`returns status code 200`, async () => {
+    const res = await request(app)
+      .post("/week/2")
+      .send({ quizNumber: 3, correctAnswers: 4, weekNumber: 2 });
+    expect(res.statusCode).toBe(200);
+  });
+  test(`checks if response returns as { quizNumber: 3, correctAnswers: 4, weekNumber: 2 }`, async () => {
+    const res = await request(app).get("/week/2");
+    expect(res.body).toEqual({ success: true, payload: expect.any(Object) });
+  });
+});
