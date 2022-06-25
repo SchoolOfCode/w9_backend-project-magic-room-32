fb2a08c02c81fbf45024a0b694527f4c5034a661
Samantha Wu <samanthayywu@gmail.com>
2022-06-25 14:01:16 +0100

    userLogin

--- models/authMods.js
+++ models/authMods.js
@@ -1,13 +1,21 @@
 import { query } from "../db/index.js";
+import { uuid } from "uuidv4";
 
 // register
-export async function userRegister(email, passowrd) {
+export async function userRegister(email, password) {
   await query(
     `INSERT INTO users (email, passowrd)
    VALUES ($1, $2)
    ON CONFLICT DO NOTHING;`,
-    [email, passowrd]
+    [email, password]
   );
 }
 
 // login
+export async function userLogin(email, password) {
+  await query(`SELECT * from users WHERE email = $1 AND password = $2`, [
+    email,
+    password,
+  ]);
+  await query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { userRegister } from "../models/authMods";
+import { userLogin, userRegister } from "../models/authMods";
+
 const authRouter = express.Router();
 
 authRouter.post('/:register', (req, res)=>{
-    res.json({success: true, payload: await userRegister(req.body.email, req.body.passowrd)})
+    res.json({success: true, payload: await userRegister(req.body.email, req.body.password)})
+})
+
+authRouter.post('/:login', (req, res)=>{
+    res.json({success: true, payload: await userLogin(req.body.email, req.body.password)})
 })
 
 export default authRouter;
